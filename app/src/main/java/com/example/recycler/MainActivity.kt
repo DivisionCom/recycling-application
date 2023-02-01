568cf795531a3fcded9dcad98c472af378e2ee6d
DivisionCom <xd6511@gmail.com>
2023-02-01 23:07:34 +0300

    button fix, add new activity with bottom navigation

--- app/src/main/java/com/example/recycler/MainActivity.kt
+++ app/src/main/java/com/example/recycler/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.recycler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+
+        btnStart.setOnClickListener{
+                val goMenu = Intent(this, MenuActivity::class.java)
+                startActivity(goMenu)
+        }
     }
 }
