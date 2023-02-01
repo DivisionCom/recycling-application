37f858e20dbe9c00b06ec3ea4557627f177c70c6
DivisionCom <xd6511@gmail.com>
2023-02-01 20:45:36 +0300

    update

--- app/src/main/java/com/example/recycler/MainActivity.kt
+++ app/src/main/java/com/example/recycler/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.recycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 }
