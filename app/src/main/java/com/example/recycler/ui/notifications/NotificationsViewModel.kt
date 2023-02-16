041e31ea981113846e502db081b2f0af98586a37
DivisionCom <xd6511@gmail.com>
2023-02-16 22:27:22 +0300

    Rebuilding initial screen, creating main interface

--- app/src/main/java/com/example/recycler/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/recycler/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Fragment in developing"
     }
     val text: LiveData<String> = _text
 }
